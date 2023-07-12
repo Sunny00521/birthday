601b73b739db2381975bf2cd91722f1a15c33827
sunny <sunny0521@proton.me>
2023-07-12 22:47:54 +0800

    feat: modify

--- assets/js/effect.js
+++ assets/js/effect.js
@@ -46,12 +46,12 @@ $("document").ready(function () {
   $("#play").click(function () {
     var audio = $(".song")[0];
     audio.play();
-    $("#bulb_yellow").addClass("bulb-glow-yellow-after");
-    $("#bulb_red").addClass("bulb-glow-red-after");
-    $("#bulb_blue").addClass("bulb-glow-blue-after");
-    $("#bulb_green").addClass("bulb-glow-green-after");
-    $("#bulb_pink").addClass("bulb-glow-pink-after");
-    $("#bulb_orange").addClass("bulb-glow-orange-after");
+    // $("#bulb_yellow").addClass("bulb-glow-yellow-after");
+    // $("#bulb_red").addClass("bulb-glow-red-after");
+    // $("#bulb_blue").addClass("bulb-glow-blue-after");
+    // $("#bulb_green").addClass("bulb-glow-green-after");
+    // $("#bulb_pink").addClass("bulb-glow-pink-after");
+    // $("#bulb_orange").addClass("bulb-glow-orange-after");
     $("body").css("backgroud-color", "#FFF");
     $("body").addClass("peach-after");
     $(this)
